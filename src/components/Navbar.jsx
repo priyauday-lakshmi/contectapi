417d567d0ecc95674f8ae4338445435f81147359
priyauday-lakshmi <priyauday130@gmail.com>
2024-03-16 21:27:01 +0800

    contextapi

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { ShoppingCart } from "phosphor-react";
+import { CartContext } from "../ContextAPI/CartContext";
+
+const Navbar = () => {
+  const { cart } = useContext(CartContext);
+  return (
+    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+      <div className="container px-4 px-lg-5">
+        
+        <h3 className="text-center heading">Shopping Cart</h3>  
+        <ul className="navbar-nav ms-auto mb-lg-0 mx-4 fs-5 " >
+          <li className="nav-item " >
+            <Link  className="text-black" to="/">
+              Shop
+            </Link>
+          </li>
+        </ul>
+
+        <Link className="text-black" to="/cart">
+          <ShoppingCart size={32} style={{ color: "#222222" }} />
+
+          <span
+            className="postion-absolute top-0 start-100 translate-middle badge  text-white rounded-pill "
+            style={{ backgroundColor: "#ff6900" }}
+          >
+            {cart.length}
+          </span>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
